1947cd57e3f58701096c96f3d9110d1e1e6e5ab6
Vitalii <kicker198686@gmail.com>
2020-09-20 15:41:08 +0300

    add main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// while (number <= 25) {
+//   console.log(number);
+//   number++;
+// }
+
+// do {
+//     console.log(number);
+//     number++;
+
+// }
+// while (number <= 25);
+// let number = 20;
+// for ( let i = 1; i <= 9; i++) {
+//     if ( i === 6){
+//     // break;
+//     continue;
+// }
+//     console.log(i);
+
+// }
+
+// for(let i = 96; i <= 100; i++) {
+
+//         console.log(i + ' Маша');
+
+// }//
+
+// let i = 5;
+// while ( i < 60 ) {
+//    console.log(i + ' deal');
+//    i++;
+// }
+
+// let i = 5;
+// do {console.log(i +' shopping whisky');
+// i++;
+// } while (i < 88);
+
+// const str = 'bvvb';
+// console.log(str.length);
